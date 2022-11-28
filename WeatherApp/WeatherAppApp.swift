0cdb5b429e5ea844ba14d8782ff4bd056167e41e
ShadowPlayer34 <khudzik24@mail.ru>
2022-11-29 00:16:23 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Андрей Худик on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
